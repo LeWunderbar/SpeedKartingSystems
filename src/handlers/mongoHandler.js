bfbcfb509c8d72a95578b23698567261b9ca80c6
m23 <maxmaxeltek@gmail.com>
2024-11-13 20:19:01 +0100

    Changed MongoDB URL

--- src/handlers/mongoHandler.js
+++ src/handlers/mongoHandler.js
@@ -7,7 +7,7 @@ require('dotenv').config({
   });
 
 module.exports = async() => {
-    await mongoose.connect(process.env.MONGODB)
+    await mongoose.connect(`mongodb://bot:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@192.168.178.96:27017/speedkarting?authSource=admin&retryWrites=true&w=majority`)
     .then(() => {
         log(`\x1b[38;2;87;117;144m[Database] \x1b[32mMongoDB\x1b[0m is connected and service Online.`)
     })
