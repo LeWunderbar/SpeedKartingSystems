10d06baa0beb9ae2d931dbbec0f56137304d6218
m23 <maxmaxeltek@gmail.com>
2024-11-19 23:40:08 +0100

    removed onVerify

--- src/events/guildMemberAdd/verifyOnJoin.js
+++ src/events/guildMemberAdd/verifyOnJoin.js
@@ -5,16 +5,17 @@ const schema = require("./../../schema/verifying");
 const log = require("./../../utils/log");
 
 module.exports = async (member) => {
-    try {
-        const data = await schema.find({ discordID: member.user.id }).exec();
-        if (member.guild.id == config.GUILD_ID) {
-            if (!data.length === 0) {
-                if (data[0].verified) {
-                    await update(data[0].robloxID, data[0].discordID)
-                }
-            }
-        }
-    } catch (err) {
-        log(`\x1b[31m[Error] \x1b[32mAn error occurred:\n\x1b[0m${err}`)
-    }
+    //
+    //try {
+    //  const data = await schema.find({ discordID: member.user.id }).exec();
+    //    if (member.guild.id == config.GUILD_ID) {
+    //        if (!data.length === 0) {
+    //            if (data[0].verified) {
+    //                await update(data[0].robloxID, data[0].discordID)
+    //            }
+    //       }
+    //    }
+    //} catch (err) {
+    //    log(`\x1b[31m[Error] \x1b[32mAn error occurred while trying to verify user on join:\n\x1b[0m${err}`)
+    //}
 }
