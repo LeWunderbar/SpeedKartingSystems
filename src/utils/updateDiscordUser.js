22355dd3dac6c6f92eefcd5b4d4044f5c55201cb
m23 <maxmaxeltek@gmail.com>
2024-11-19 23:29:56 +0100

    uodate Fix

--- src/utils/updateDiscordUser.js
+++ src/utils/updateDiscordUser.js
@@ -22,11 +22,7 @@ async function update(RobloxUserId, DiscordUserId) {
             return 0
         }
 
-        console.log(member.roles.highest.position)
-        console.log(botMember.roles.highest.position)
-
-        if (member.roles.highest.position >= member.roles.highest.position) {
-            console.log("postionErr")
+        if (member.roles.highest.position >= botMember.roles.highest.position) {
             return "postionErr"
         }
 
