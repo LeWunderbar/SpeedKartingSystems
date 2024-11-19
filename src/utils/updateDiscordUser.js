949a62e9fa073c0299e6a847387ddc9f4043e2c5
m23 <maxmaxeltek@gmail.com>
2024-11-19 22:05:30 +0100

    Chnages to update Function

--- src/utils/updateDiscordUser.js
+++ src/utils/updateDiscordUser.js
@@ -11,50 +11,60 @@ async function update(RobloxUserId, DiscordUserId) {
         const guild = client.guilds.cache.get(config.GUILD_ID)
         const username = await getUsernameFromUserId(RobloxUserId)
         const member = await guild.members.fetch(DiscordUserId)
-        if (member.id != guild.ownerId) {
-            member.setNickname(username)
+        if (member.id == guild.ownerId) {
+            return "OwnerErr"
+        } 
 
-            if (!development) {
-                // Roles based on group rank
-                // RP
-                const RP_RankName = await getUserGroupRank(RobloxUserId, "16117955")
-                if (RP_RankName != null) {
-                    if (ranklistGroup[RP_RankName]) {
-                        try {
-                            member.roles.add(ranklistGroup[RP_RankName])
-                        } catch (err) {
-                            log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
-                        }
-                    }
+        if (development) {
+            return 0
+        }
+
+        if (member.roles.highest.position >= client.roles.highest.position) {
+            return "postionErr"
+        }
+
+        member.setNickname(username)
+        // Roles based on group rank
+        // RP
+        const RP_RankName = await getUserGroupRank(RobloxUserId, "16117955")
+        if (RP_RankName != null) {
+            if (ranklistGroup[RP_RankName]) {
+                try {
+                    member.roles.add(ranklistGroup[RP_RankName])
+                } catch (err) {
+                    log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
                 }
-    
-                // QA
-                const QA_RankName = await getUserGroupRank(RobloxUserId, "35177988")
-                if (QA_RankName != null) {
-                    if (ranklistGroup[QA_RankName]) {
-                        try {
-                            member.roles.add(ranklistGroup[QA_RankName])
-                        } catch (err) {
-                            log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
-                        }
-                    }
-                }
-    
-                // Mod
-                const Mod_RankName = await getUserGroupRank(RobloxUserId, "35177986")
-                if (Mod_RankName != null) {
-                    if (ranklistGroup[Mod_RankName]) {
-                        try {
-                            member.roles.add(ranklistGroup[Mod_RankName])
-                        } catch (err) {
-                            log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
-                        }
-                    }
-                }
-    
-                // Roles based on in-game rank
             }
         }
+    
+        // QA
+        const QA_RankName = await getUserGroupRank(RobloxUserId, "35177988")
+        if (QA_RankName != null) {
+            if (ranklistGroup[QA_RankName]) {
+                try {
+                    member.roles.add(ranklistGroup[QA_RankName])
+                } catch (err) {
+                    log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
+                }
+            }
+        }
+    
+        // Mod
+        const Mod_RankName = await getUserGroupRank(RobloxUserId, "35177986")
+        if (Mod_RankName != null) {
+            if (ranklistGroup[Mod_RankName]) {
+                try {
+                    member.roles.add(ranklistGroup[Mod_RankName])
+                } catch (err) {
+                    log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
+                }
+            }
+        }
+
+        // Roles based on in-game rank
+
+        return 0
+
     } catch (err) {
         log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
     }
