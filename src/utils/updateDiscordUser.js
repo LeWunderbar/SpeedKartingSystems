25c41548b809876a45b93cc7c415174bc2634c18
m23 <maxmaxeltek@gmail.com>
2024-11-19 21:56:53 +0100

    Changes

--- src/utils/updateDiscordUser.js
+++ src/utils/updateDiscordUser.js
@@ -13,49 +13,47 @@ async function update(RobloxUserId, DiscordUserId) {
         const member = await guild.members.fetch(DiscordUserId)
         if (member.id != guild.ownerId) {
             member.setNickname(username)
-        }
 
-        if (!development) {
-            // Roles based on group rank
-            // RP
-            const RP_RankName = await getUserGroupRank(RobloxUserId, "16117955")
-            if (RP_RankName != null) {
-                if (ranklistGroup[RP_RankName]) {
-                    try {
-                        member.roles.add(ranklistGroup[RP_RankName])
-                    } catch (err) {
-                        console.log(err)
+            if (!development) {
+                // Roles based on group rank
+                // RP
+                const RP_RankName = await getUserGroupRank(RobloxUserId, "16117955")
+                if (RP_RankName != null) {
+                    if (ranklistGroup[RP_RankName]) {
+                        try {
+                            member.roles.add(ranklistGroup[RP_RankName])
+                        } catch (err) {
+                            log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
+                        }
                     }
                 }
-            }
-
-            // QA
-            const QA_RankName = await getUserGroupRank(RobloxUserId, "35177988")
-            if (QA_RankName != null) {
-                if (ranklistGroup[QA_RankName]) {
-                    try {
-                        member.roles.add(ranklistGroup[QA_RankName])
-                    } catch (err) {
-                        console.log(err)
+    
+                // QA
+                const QA_RankName = await getUserGroupRank(RobloxUserId, "35177988")
+                if (QA_RankName != null) {
+                    if (ranklistGroup[QA_RankName]) {
+                        try {
+                            member.roles.add(ranklistGroup[QA_RankName])
+                        } catch (err) {
+                            log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
+                        }
                     }
                 }
-            }
-
-            // Mod
-            const Mod_RankName = await getUserGroupRank(RobloxUserId, "35177986")
-            if (Mod_RankName != null) {
-                if (ranklistGroup[Mod_RankName]) {
-                    try {
-                        member.roles.add(ranklistGroup[Mod_RankName])
-                    } catch (err) {
-                        console.log(err)
+    
+                // Mod
+                const Mod_RankName = await getUserGroupRank(RobloxUserId, "35177986")
+                if (Mod_RankName != null) {
+                    if (ranklistGroup[Mod_RankName]) {
+                        try {
+                            member.roles.add(ranklistGroup[Mod_RankName])
+                        } catch (err) {
+                            log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
+                        }
                     }
                 }
+    
+                // Roles based on in-game rank
             }
-
-            // Roles based on in-game rank:
-
-
         }
     } catch (err) {
         log(`\x1b[31m[Error] \x1b[32m"An error occurred in UpdateDiscordUser.js:"\n\x1b[0m${err}`)
